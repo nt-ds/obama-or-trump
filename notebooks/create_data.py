b23c7b85a298e24dacdbeb62d9c8adf4cf46c7e6
nt-ds <ngoct76@gmail.com>
2020-04-28 16:05:27 -0400

    Retrieve tweets

---
+++ notebooks/create_data.py
@@ -0,0 +1,8 @@
+# --- Import libraries ---
+import get_tweets
+
+
+# --- Create data ---
+# get tweets from president Obama and President Trump
+get_tweets.get_tweets("BarackObama")
+get_tweets.get_tweets("realDonaldTrump")
